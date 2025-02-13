54f253bd9a60d11db54f8dd2ee320170f0f31fa5
Julen Altuna <jualtuna4@gmail.com>
2025-02-13 23:21:06 +0100

    add functions example and exercise

---
+++ Chapter01/fahrenheitCelsiusConFunciones.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* EJERCICIO 1-15: Escriba de nuevo el programa de conversión de temperatura de la sección 1.2, de modo que
+ * utilice una función para la conversión */
+
+/* Imprime la tabla Fahrenheit-Celsius para fahr = 0, 20, ..., 300 */
+
+float conversion(float fahr);
+
+int main() {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;    /* limite inferior de la tabla de temperaturas */
+	upper = 300;    /* limite superior */
+	step = 20;    /* tamaño del incremento */
+
+	fahr = lower;
+
+	while (fahr <= upper) {
+		printf("%3.0f %6.1f\n", fahr, conversion(fahr));
+		fahr = fahr + step;
+	}
+}
+
+float conversion(float fahr) {
+  	float celsius;
+
+	celsius = (5.0/9.0) * (fahr-32.0);
+
+    return celsius;
+}
---
+++ Chapter01/funciones.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int power(int m, int n);
+
+int main() {
+  	int i;
+
+    for (i = 0; i < 10; i++)
+      	printf("%d %d %d\n", i, power(2, i), power(-3, i));
+
+    return 0;
+}
+
+/* power: eleva la base a la n-ésima potencia; n >= 0 */
+int power(int base, int n) {
+  	int i, p;
+
+    p = 1;
+    for (i = 0; i < n; i++)
+      	p = p * base;
+
+    return p;
+}
+
+
+/* power: eleva la base a la n-ésima potencia; n >= 0
+ * (versión en estilo antiguo)
+ *
+power (base, n)
+int base, n;
+{
+	int i, p;
+
+	p = 1;
+	for (i = 0; i < n; i++)
+		p = p * base;
+
+	return p;
+} */
